1f9da3299b990211a1bb1011297b01a51aaafcac
Peter <peter_s_96@live.com>
2022-07-21 14:25:24 -0700

    basic search bar

--- src/routes/components/SearchBar.js
+++ src/routes/components/SearchBar.js
@@ -1,18 +1,37 @@
 import { useState } from "react";
 
+const BASE_URL = "https://sensationnel-maison-12931.herokuapp.com/api";
+
 const SearchBar = () => {
-    const [searchString, setSearchString] = useState('');
+    const [searchQuery, setSearchQuery] = useState('');
     const changeHandler = e => {
-        setSearchString(e.target.value)
+        setSearchQuery(e.target.value)
     }
-    const submitHandler = e => {
+
+    async function searchRequest(searchstring){
+        try{
+            const response = await fetch (`${BASE_URL}/search/${searchstring}`,{ //
+                headers: {
+                    "Content-Type": "application/json",
+                }
+            })
+            const data = await response.json();
+            return data;
+        } catch(error) {
+            console.log(error);
+        }
+    };
+
+    const submitHandler = async (e) => {
         e.preventDefault();
+        const result = await searchRequest(searchQuery);
+        console.log(result);
     }
 
 
 
     return(
-        <form onSubmit={}>
+        <form onSubmit={submitHandler}>
         <input type="search" placeholder="Search by title..." onChange={changeHandler}/>
         <button type="submit">Search</button>
         </form>
