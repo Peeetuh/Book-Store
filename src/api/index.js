22d0a40a1cebe38a7b1849807e2ce8ce1c4305df
Peter <peter_s_96@live.com>
2022-07-23 12:23:47 -0700

    moved fetch call from home page to api index

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,9 @@
 const BASE_URL = "https://sensationnel-maison-12931.herokuapp.com/api";
 
+
+  
+  
+
 export const fetchRegister = async (username, password) => {
   try {
     const response = await fetch(`${BASE_URL}/users/register`, {
@@ -56,3 +60,35 @@ export const fetchUserAccount = async (token) => {
   }
 }
 
+export const requestCuratedRanking = async () => {
+  const response = await fetch(`${BASE_URL}/books/lists/curated-rankings`,{
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }); 
+  const data = await response.json();
+  // console.log(data);
+  return data;
+}
+
+export const requestTopRated = async () => {
+  const response = await fetch(`${BASE_URL}/books/lists/curated-ratings`, {
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+  const dataTopRated = await response.json();
+  console.log(dataTopRated);
+  return dataTopRated;
+}
+
+export const requestFeatured = async () => {
+  const response = await fetch(`${BASE_URL}/books/lists/featured`, {
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+  const dataFeatured = await response.json();
+  console.log(dataFeatured);
+  return dataFeatured;
+}
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,45 +2,13 @@ import { useEffect, useState } from "react";
 import DisplayCuratedRanking from "./components/DisplayCuratedRanking";
 import DisplayHighestRanking from "./components/DisplayHighestRanking";
 import DisplayFeatured from "./components/DisplayFeatured";
+import { requestCuratedRanking, requestTopRated, requestFeatured } from "../api";
 
 function Home() {
   const [topCuratedRanking, setTopCuratedRanking] = useState([]);
   const [topRated, setTopRated] = useState([]);
   const [featured, setFeatured] = useState([]);
 
-    const BASE_URL = "https://sensationnel-maison-12931.herokuapp.com/api";
-    const requestCuratedRanking = async () => {
-      const response = await fetch(`${BASE_URL}/books/lists/curated-rankings`,{
-        headers: {
-          "Content-Type": "application/json"
-        }
-      }); 
-      const data = await response.json();
-      // console.log(data);
-      return data;
-    }
-
-    const requestTopRated = async () => {
-      const response = await fetch(`${BASE_URL}/books/lists/curated-ratings`, {
-        headers: {
-          "Content-Type": "application/json"
-        }
-      });
-      const dataTopRated = await response.json();
-      console.log(dataTopRated);
-      return dataTopRated;
-    }
-
-    const requestFeatured = async () => {
-      const response = await fetch(`${BASE_URL}/books/lists/featured`, {
-        headers: {
-          "Content-Type": "application/json"
-        }
-      });
-      const dataFeatured = await response.json();
-      console.log(dataFeatured);
-      return dataFeatured;
-    }
 
     useEffect(() => {
         const fetchCuratedRanking = async () => {
@@ -57,7 +25,7 @@ function Home() {
           const fetchFeatured = async () => {
             const resultFeatured = await requestFeatured();
             console.log(resultFeatured);
-            setFeatured(resultFeatured);
+            setFeatured(resultFeatured[0]);
           }
           fetchFeatured();
     },[])
