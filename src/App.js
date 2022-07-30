e2b5a0ca37a1c0cf46029437d22265192b2c2cd9
Tim Kent <timdekent@gmail.com>
2022-07-30 13:17:28 -0700

    reworked genres: routes now use params, removed unnecessary routes and links, results paginate, basic styling

--- src/App.js
+++ src/App.js
@@ -2,23 +2,24 @@ import { Link, Route, Routes } from "react-router-dom";
 import React from "react";
 import Home from "./routes/Home";
 import { useState } from "react";
-import { MyAccount, Login, Register, GuestCart, UserCart, SingleBookPage } from "./routes";
+import {
+  MyAccount,
+  Login,
+  Register,
+  GuestCart,
+  UserCart,
+  SingleBookPage,
+} from "./routes";
 import "./App.css";
 import SearchBar from "./routes/components/SearchBar";
-import Genre from "./routes/Genre";
-import Horror from "./routes/components/Horror";
-import ScienceFiction from "./routes/components/ScienceFiction";
-import GeneralFiction from "./routes/components/GeneralFiction";
-import Mystery from "./routes/components/Mystery";
-import Comedy from "./routes/components/Comedy";
-import Romance from "./routes/components/Romance";
-import Thriller from "./routes/components/Thriller";
+import Genres from "./routes/Genres";
+import DisplayGenreBooks from "./routes/components/DisplayGenreBooks";
 import SearchResult from "./routes/components/SearchResult";
 import Admin from "./routes/Admin";
 import AdminUsers from "./routes/AdminUsers";
 import AdminOrders from "./routes/AdminOrders";
 import AdminProducts from "./routes/AdminProducts";
-import img1 from "./routes/components/Images/logo.png"
+import img1 from "./routes/components/Images/logo.png";
 
 function App() {
   const [searchResult, setSearchResult] = useState([]);
@@ -29,90 +30,108 @@ function App() {
   const [password, setPassword] = useState("");
   const [token, setToken] = useState(window.localStorage.getItem("token"));
   const [userId, setUserId] = useState(window.localStorage.getItem("userId"));
+  const [genres] = useState(['Horror', 'Science-Fiction', 'Thriller', 'General Fiction', 'Comedy', 'Romance', 'Mystery'])
+  const [genreSelect, setGenreSelect] = useState("");
   return (
     <div className="App">
-        <nav className="nav-bar">
-         <img src={img1} alt="logo" className="logo"/>
-          <Link className="links" to="/">Home</Link>
-          <Link className="links" to="/Genre">Genre</Link>
-          {token ? null : <Link className="links" to="/Login">Login</Link>}
-          {token ? null : <Link className="links" to="/Register">Register</Link>}
-          {token ? <Link className="links" to="/MyAccount">My Account</Link> : null}
-          {token ? (
-            <Link to={`/${userId}/cart`}>Cart</Link>
-          ) : (
-            <Link to="/GuestCart">Cart</Link>
-          )}
-          {token && userData.isAdmin && <Link to="/admin">Admin</Link>}
-          <div className="nav-bar-search">
-            <SearchBar setSearchResult={setSearchResult} />
-          </div>
-        </nav>
-        <Routes>
-          <Route path="/" element={<Home userId={userId} token={token} />} />
+      <nav className="nav-bar">
+        <img src={img1} alt="logo" className="logo" />
+        <Link className="links" to="/">
+          Home
+        </Link>
+        <Link className="links" to="/Genres">
+          Genres
+        </Link>
+        {token ? null : (
+          <Link className="links" to="/Login">
+            Login
+          </Link>
+        )}
+        {token ? null : (
+          <Link className="links" to="/Register">
+            Register
+          </Link>
+        )}
+        {token ? (
+          <Link className="links" to="/MyAccount">
+            My Account
+          </Link>
+        ) : null}
+        {token ? (
+          <Link to={`/${userId}/cart`}>Cart</Link>
+        ) : (
+          <Link to="/GuestCart">Cart</Link>
+        )}
+        {token && userData.isAdmin && <Link to="/admin">Admin</Link>}
+        <div className="nav-bar-search">
+          <SearchBar setSearchResult={setSearchResult} />
+        </div>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home userId={userId} token={token} />} />
+        <Route
+          path="Login"
+          element={
+            <Login
+              setToken={setToken}
+              setUserData={setUserData}
+              username={username}
+              setUsername={setUsername}
+              password={password}
+              setPassword={setPassword}
+              setUserId={setUserId}
+            />
+          }
+        />
+        <Route
+          path="Register"
+          element={
+            <Register
+              setToken={setToken}
+              username={username}
+              setUsername={setUsername}
+              password={password}
+              setPassword={setPassword}
+              setUserId={setUserId}
+            />
+          }
+        />
+        <Route
+          path="MyAccount"
+          element={
+            <MyAccount
+              token={token}
+              setToken={setToken}
+              setUsername={setUsername}
+              username={username}
+              setUserId={setUserId}
+            />
+          }
+        />
+        <Route path="/genres" element={<Genres genres={genres} setGenreSelect={setGenreSelect} />} />
+        <Route path="/genres/:genre" element={<DisplayGenreBooks genreSelect={genreSelect} />} />
+        <Route path="GuestCart" element={<GuestCart />} />
+        <Route
+          path={`/${userId}/cart`}
+          element={<UserCart username={username} token={token} />}
+        />
+        <Route
+          path="books/:bookId"
+          element={<SingleBookPage token={token} userId={userId} />}
+        />
+        <Route path="/admin" element={<Admin />}>
+          <Route path="/admin/users" element={<AdminUsers token={token} />} />
+          <Route path="/admin/orders" element={<AdminOrders token={token} />} />
           <Route
-            path="Login"
-            element={
-              <Login
-                setToken={setToken}
-                setUserData={setUserData}
-                username={username}
-                setUsername={setUsername}
-                password={password}
-                setPassword={setPassword}
-                setUserId={setUserId}
-              />
-            }
+            path="/admin/products"
+            element={<AdminProducts token={token} />}
           />
-          <Route
-            path="Register"
-            element={
-              <Register
-                setToken={setToken}
-                username={username}
-                setUsername={setUsername}
-                password={password}
-                setPassword={setPassword}
-                setUserId={setUserId}
-              />
-            }
-          />
-          <Route
-            path="MyAccount"
-            element={
-              <MyAccount
-                token={token}
-                setToken={setToken}
-                setUsername={setUsername}
-                username={username}
-                setUserId={setUserId}
-              />
-            }
-          />
-          <Route path="/Genre" element={<Genre />} />
-          <Route path="GuestCart" element={<GuestCart />} />
-          <Route
-            path={`/${userId}/cart`}
-            element={<UserCart username={username} token={token} />}
-          />
-          <Route path="books/:bookId" element={<SingleBookPage token={token} userId={userId}/>}/>
-          <Route path="/Horror" element={<Horror />} />
-          <Route path="/ScienceFiction" element={<ScienceFiction />} />
-          <Route path="/GeneralFiction" element={<GeneralFiction />} />
-          <Route path="/Mystery" element={<Mystery />} />
-          <Route path="/Thriller" element={<Thriller />} />
-          <Route path="/Comedy" element={<Comedy />} />
-          <Route path="/Romance" element={<Romance />} />
-          <Route path="/admin" element={<Admin />}>
-            <Route path="/admin/users" element={<AdminUsers token={token} />} />
-            <Route path="/admin/orders" element={<AdminOrders token={token} />} />
-            <Route path="/admin/products" element={<AdminProducts token={token} />} />
-          </Route>
-          <Route
-            path="/SearchResult"
-            element={<SearchResult searchResult={searchResult} />}
-          />
-        </Routes>
+        </Route>
+        <Route
+          path="/SearchResult"
+          element={<SearchResult searchResult={searchResult} />}
+        />
+      </Routes>
     </div>
   );
 }
