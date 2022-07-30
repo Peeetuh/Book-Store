6acadcd3f6d24ab4eb8522b02876940afee27f5a
Joseph Martelli <joseph.martelli4@gmail.com>
2022-07-30 09:27:46 -0700

    fake bio, fake pic, books clickable, routes work, userID is undefined

--- src/routes/components/SingleAuthor.js
+++ src/routes/components/SingleAuthor.js
@@ -1,10 +1,18 @@
 import { useEffect, useState } from "react";
 import { requestAuthor } from "../../api";
 import { useParams } from "react-router-dom";
+import { faker } from '@faker-js/faker';
+import CartForm from "./CartForm";
+import { Link, Route, Routes } from "react-router-dom";
+
+
 
 function Author(){
     const [author, setAuthor] = useState([]);
     const { authorName } = useParams()
+    const fakeBio = faker.lorem.paragraph(6);
+    const fakePic = faker.image.people( 600, 400, true ); 
+    
 
     useEffect(()=> {
         const fetchAuthorData = async () => {
@@ -19,22 +27,33 @@ function Author(){
         <h3>{ authorName } </h3>
         <img className="authorPic"
         style={{width:400, height:400}}
-        src="https://i.imgur.com/rwZzeOM.png" alt="awful"></img>
+        src={fakePic} alt="awful"></img>
         <p>
-            
+          <strong> About author:</strong> {fakeBio}
         </p>
         </header>
         <div className="authorPage">
         {author.map(book =>{
         return(
+            <>
             <div key={book.id}>
-                <img src={book.imageLinkS} alt={book.title}/>
+              <Link to={`/${book.id}`}>
+                <img src={book.imageLinkM} alt={book.title} />
+              </Link>
+              <Link to={`/authors/${book.author}`}>
+              <p> By {book.author}</p>
+              </Link>
+              <CartForm  />
             </div>
-        )
-        })}
-            </div>
-    </section>
-    )
+            <Routes>
+              <Route path={`/${book.id}`} />
+            </Routes>
+          </>
+        );
+      })}
+    </div>
+  </section>
+);
 };
 
 export default Author;
