12ae00fec1f18fe734e4b54bbad2451128562b92
Joseph Martelli <joseph.martelli4@gmail.com>
2022-08-03 19:45:10 -0700

    form created, can't get inputs to be defined it seems :(

--- src/routes/MyAccount.js
+++ src/routes/MyAccount.js
@@ -3,7 +3,11 @@ import { fetchUserAccount } from "../api";
 
 const MyAccount = ({ token, username }) => {
   const [myAccount, setMyAccount] = useState([]);
-
+  const [editMode, setEditMode] = useState(false);
+  const [state, setState] = useState();
+  const [city, setCity] = useState();
+  const [street, setStreet] = useState();
+  const [zip, setZip] = useState();
   useEffect(() => {
     const loadMyAccount = async () => {
       const fetchedAccount = await fetchUserAccount(token);
@@ -11,10 +15,62 @@ const MyAccount = ({ token, username }) => {
     };
     loadMyAccount();
   }, [token]);
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setEditMode(true);
+    console.log(editMode);
+  }
+  const addressHandler = (e) => {
+    e.preventDefault();
+    const address = { state, city, street, zip}
+    console.log(address)
+    return address;
+  }
 
   return (
-    <>
+<>
       <h1>Welcome {username}!</h1>
+      <form onSubmit={submitHandler}>
+      <button type="submit"> Edit Profile</button>
+      </form>
+      {editMode && (<>
+      <h3> Set address details!</h3><form>
+        <label>
+          State:
+        </label>
+        <input 
+        type="text" 
+        required 
+        value={state}
+        onChange={(e) => setState(e.target.value)}
+        />
+        <label>
+          City:
+        </label>
+        <input 
+        type="text" 
+        required 
+        value={city}
+        onChange={(e) => setCity(e.target.value)} />
+        <label>
+          Street:
+        </label>
+        <input 
+        type="text" 
+        required 
+        value={street}
+        onChange={(e) => setStreet(e.target.value)}/>
+        <label>
+          Zip:
+        </label>
+        <input 
+        type="text" 
+        required 
+        value={zip}
+        onChange={(e) => setZip(e.target.value)} />
+        <button onSubmit={addressHandler}>Confirm address</button>
+      </form></>
+      )}
       {myAccount.length < 1 ? (
         <h6>Your order history will appear here</h6>
       ) : (
